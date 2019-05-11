40e51ed2e06888b39a7075b822429ed29631bf4a
Simranjit Singh <sjsingh@gmail.com>
2019-05-11 19:40:13 +1000

    Added bars editor - showing just the content

--- src/Bar/BarsEditor.js
+++ src/Bar/BarsEditor.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import styles from './Bar.module.css';
 
-function BarsEditor(props) {
-  return <div>BarsEditor goes here</div>;
+function BarValueButton({ value, onRemoveBarValue }) {
+  return (
+    <div className={styles.barValueButton}>
+      {value}
+      <button onClick={onRemoveBarValue}>x</button>
+    </div>
+  );
+}
+
+function onRemove(value, id) {
+  console.log(`Remove ${value} from id: ${id}`);
+}
+
+function BarsEditor({ bars }) {
+  if (bars && bars.length > 0) {
+    const barValuesRenderer = bars.map(item => {
+      return (
+        <div className={styles.barsItemContainer} key={item.id}>
+          <span>{item.name}</span>
+          {item.values.map((value, index) => {
+            const onRemoveBarValue = onRemove.bind(this, value, item.id);
+            return (
+              <BarValueButton
+                key={index}
+                value={value}
+                onRemoveBarValue={onRemoveBarValue}
+              />
+            );
+          })}
+        </div>
+      );
+    });
+
+    return (
+      <React.Fragment>
+        <div>BarsEditor goes here</div>
+        {barValuesRenderer}
+      </React.Fragment>
+    );
+  }
+
+  return null;
 }
 
 export default BarsEditor;
